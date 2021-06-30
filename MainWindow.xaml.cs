d10c7286d5fd4197484c4db2e5dde3e5451564d8
krystian_sadowski <20336369+krys-codes@users.noreply.github.com>
2021-06-30 10:53:23 +0200

    Updated project
    Project updated.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp1
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Sort list ascending
+            FontList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Source", System.ComponentModel.ListSortDirection.Ascending));
+        }
+    }
+}
